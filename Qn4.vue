7bac479e16572fa9063a430e3ab66fad5ba6a452
subash-500apps <102018284+subash-500apps@users.noreply.github.com>
2022-04-12 17:14:14 +0530

    Add files via upload

---
+++ Qn4.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <p>Qn4</p>
+    </div>
+</template>
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+    setup() {
+        
+    },
+})
+</script>
